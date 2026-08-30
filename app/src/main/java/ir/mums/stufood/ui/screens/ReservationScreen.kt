c646d1090aec596d1871b7c13f0055190521a794
Behroz666 <BehrozBesharatnia@gmail.com>
2026-08-30 17:58:22 +0330

    fix

--- app/src/main/java/ir/mums/stufood/ui/screens/ReservationScreen.kt
+++ app/src/main/java/ir/mums/stufood/ui/screens/ReservationScreen.kt
@@ -52,7 +52,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenu
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.HorizontalDivider
@@ -1035,7 +1035,7 @@ private fun DropdownField(
         }
 
         // The actual dropdown menu list
-        ExposedDropdownMenu(
+        DropdownMenu(
             expanded = expanded,
             onDismissRequest = { expanded = false }
         ) {
