aa9a530ec7d860a3669614f5d6d0db21dc486afd
Behroz666 <BehrozBesharatnia@gmail.com>
2026-09-01 07:58:38 +0330

    add back the removed dependecies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,5 +74,24 @@ android {
 }
 
 dependencies {
-    // unchanged
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.activity.compose)
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.extended)
+    implementation(libs.androidx.navigation.compose)
+
+    implementation(libs.okhttp)
+    implementation(libs.jsoup)
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.coil.compose)
+    implementation(libs.kotlinx.serialization.json)
+
+    debugImplementation(libs.androidx.ui.tooling)
 }
